f3ca4fd4a677adcace82b86f6baf2195788f1887
marti <martinagarciaa012@gmail.com>
2021-06-17 14:18:50 -0300

    inital

--- src/main/java/ejemplo_con_grafica/interfazGrafica/ElegirFigura.java
+++ src/main/java/ejemplo_con_grafica/interfazGrafica/ElegirFigura.java
@@ -14,34 +14,17 @@ import javax.swing.JLabel;
 public class ElegirFigura extends JFrame {
 
     private JPanel contentPane;
-    private JLabel lblNewLabel;
-    private final JButton continuar = new JButton("Continuar");
-    private JLabel lblNewLabel_1;
+    private JLabel lblNewLabel; //del icono
+    private JButton continuar;
+    private JLabel lblNewLabel_1; //de texto
+    private JComboBox<String> comboBox;
 
 
     public ElegirFigura(Icon icon) {
 
-        if (icon == null) {
-            lblNewLabel = new JLabel();
-        } else
-            lblNewLabel = new JLabel(icon);
-
-
-        lblNewLabel.setBounds(243, 38, 352, 253);
-        lblNewLabel.setLabelFor(lblNewLabel);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 638, 382);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-
-        JComboBox<String> comboBox = new JComboBox<>();
-        comboBox.addItem("CIRCULO");
-        comboBox.addItem("TRIANGULO");
-        comboBox.addItem("CUADRADO");
-        comboBox.setBounds(30, 90, 173, 21);
-        contentPane.add(comboBox);
-
+        establecerIcono(icon);
+        establecerPropiedades();
+        propiedadesComboBox();
 
         continuar.setBounds(30, 134, 173, 21);
         continuar.addActionListener(e -> {
@@ -58,10 +41,6 @@ public class ElegirFigura extends JFrame {
         contentPane.add(continuar);
         contentPane.add(lblNewLabel);
 
-        lblNewLabel_1 = new JLabel("Ingrese la figura que desea crear: ");
-        lblNewLabel_1.setBounds(30, 41, 203, 13);
-        contentPane.add(lblNewLabel_1);
-
 
     }
 
@@ -76,5 +55,37 @@ public class ElegirFigura extends JFrame {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
 
+    }
+
+    private void establecerIcono(Icon icono) {
+        if (icono == null) {
+            lblNewLabel = new JLabel();
+        } else
+            lblNewLabel = new JLabel(icono);
+    }
+
+    private void establecerPropiedades() {
+        lblNewLabel.setBounds(243, 38, 352, 253);
+        lblNewLabel.setLabelFor(lblNewLabel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 638, 382);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+        lblNewLabel_1 = new JLabel("Ingrese la figura que desea crear: ");
+        lblNewLabel_1.setBounds(30, 41, 203, 13);
+        contentPane.add(lblNewLabel_1);
+
+        continuar = new JButton("Continuar");
+    }
+
+    private void propiedadesComboBox() {
+        comboBox = new JComboBox<>();
+        comboBox.addItem("CIRCULO");
+        comboBox.addItem("TRIANGULO");
+        comboBox.addItem("CUADRADO");
+        comboBox.setBounds(30, 90, 173, 21);
+        contentPane.add(comboBox);
     }
 }
